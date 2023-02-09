eaa49462ffe1377411d27ef3e98ff483a23f74b0
Thuan Pham <thuanowa@gmail.com>
2023-02-09 22:12:06 +0700

    build: plasmo frameswork init

---
+++ newtab.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function IndexNewtab() {
+  const [data, setData] = useState("")
+
+  return (
+    <div>
+      <p>when will i die</p>
+    </div>
+  )
+}
+
+export default IndexNewtab
